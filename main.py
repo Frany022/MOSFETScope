59e9f21123a78021d44ce2c132dd890483338f9b
Tibi <bajortibor02@gmail.com>
2026-04-21 15:06:10 +0200

    Update main.py

--- main.py
+++ main.py
@@ -56,6 +56,137 @@ def output_char(split_types, nums, data_number):
     @cursor.connect("add")
     def on_add(sel):
         i = int(round(sel.index))
+        sel.annotation.set_text(f"RDSon={RDSon[i]:.3f}{resistance} VGS={VGS[i]:.0f}V VDS={VDS[i]}V IDS={IDS[i]}A")
+
+    plt.xlabel("VDS (V)")
+    plt.ylabel("IDS (A)")
+    plt.legend(labels='RDSon')
+    plt.show()
+
+
+def IGSS(split_types):
+    if "IG" in split_types:
+        y_index = split_types.index("IG")
+    elif "IGabs" in split_types:
+        y_index = split_types.index("IGabs")
+    else:
+        print("no IG, not able to plot")
+        return None
+
+    if "VGS" in split_types: 
+        x_index = split_types.index("VGS")
+    else: 
+        x_index = None
+    
+    return y_index, x_index
+
+def quadrant(split_types):
+    if "IDS" in split_types:
+        y_index = split_types.index("IDS")
+    else:
+        y_index = None
+
+    if "VDS" in split_types:
+        x_index = split_types.index("VDS")
+    else:
+        x_index = None
+
+    return y_index, x_index
+
+
+with open(filename, 'r') as reader:
+    csv_file = csv.reader(filename, delimiter=',')
+    for row in reader:
+        if DataValue in row:
+            data.append(row)
+            data_number += 1
+        elif DataName in row:
+            data.append(row)
+        else:
+            pass
+reader.close
+
+for type1 in range(len(DataName)): 
+    type1 = data[0].replace(DataName + Removable + " ", "")
+
+
+type_number = [y for y in type1.split("," + " ")]
+
+nums = [[0 for i in range(len(type_number))] for j in range(data_number)]
+
+#data_number kettő lesz ebben az esteben és len(type_number) 4
+
+for i in range(data_number + 1):
+    if i == 0:
+        continue
+    value = data[i].replace(DataValue + Removable + " ", "")
+
+    split_values = value.split(", ")
+
+    nums[i - 1] = [float(x) for x in split_values]
+
+
+split_types = type1.split(", ")
+
+#print("split types: ", split_types) checking for the exact name
+output_char(split_types, nums, data_number)import csv
+import matplotlib.pyplot as plt
+from array import *
+import mplcursors
+
+filename = "test3.csv"
+DataName = "DataName"
+DataValue = "DataValue"
+Removable = ","
+data = []
+data_number = 0
+
+def output_char(split_types, nums, data_number):
+    
+    if "IDS" in split_types:
+        IDS_index = split_types.index("IDS")
+    else:
+        print("output char not available because no IDS")
+        return None
+    
+    if "VDS" in split_types:
+        VDS_index = split_types.index("VDS")
+    else:
+        print("output char not available because no VDS")
+        return None
+
+    for item in split_types:
+        match item:
+            case "Rdson" | "Rdson\n" | "RDSon" | "RDSon\n":
+                rdson = split_types.index(item)
+                break
+    
+    if "VGS" in split_types:
+        VGS_index = split_types.index("VGS")
+    
+    IDS = []
+    VDS = []
+    RDSon = []
+    VGS = []
+    resistance = "ohm"
+
+    for i in range(data_number):
+        IDS.append(nums[i][IDS_index])
+        VDS.append(nums[i][VDS_index])
+        RDSon.append(nums[i][rdson-1])
+        VGS.append(nums[i][VGS_index])
+
+    for i in range(data_number):
+            if RDSon[i] < 1:
+                RDSon[i] = RDSon[i] * 1000
+                resistance = "mhom"
+
+    plt.plot(VDS, IDS)
+
+    cursor = mplcursors.cursor(hover=True)
+    @cursor.connect("add")
+    def on_add(sel):
+        i = int(round(sel.index))
         sel.annotation.set_text(f"RDSon={RDSon[i]:.3f}{resistance} VGS={VGS[i]:.0f}V")
 
     plt.xlabel("VDS (V)")
