668789a66f2075e88902efbe3fedea4c1421249c
Tibi <bajortibor02@gmail.com>
2026-05-21 16:50:29 +0200

    Update rdson.py

--- rdson.py
+++ rdson.py
@@ -1,2 +1,30 @@
+import csv
+import numpy as np
+
+def read_csv():
+    data = []
+    count = 0
+    from_file = {
+        "filename": "test3.csv",
+        "DataName" : "DataName",
+        "DataValue" : "DataValue"
+    }
+    with open(from_file["filename"], "r") as reader:
+        csv_file = csv.reader(from_file["filename"], delimiter=',')
+        for row in reader:
+            if from_file["DataValue"] in row:
+                data.append(row)
+                count += 1
+            elif from_file["DataName"] in row:
+                data.append(row)
+            else:
+                pass
+    reader.close
+    parser(data, from_file)
+    return data, count
+
+def parser(data, from_file):
+    pass
+
 def char():
     pass
