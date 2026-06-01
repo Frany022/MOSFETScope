88fe738ac3d67639683952759dba025bdd9e8bf2
Tibi <bajortibor02@gmail.com>
2026-06-01 14:05:33 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,28 +2,46 @@ import rdson
 import igs
 import quadrant
 import csv
+import file_selector
+
+DATA_NAME = "DataName"
+DATA_VALUE = "DataValue"
 
 
 def main():
     data = []
+    names = []
     count = 0
-    from_file = {
-        "filename": "test3.csv",
-        "DataName" : "DataName",
-        "DataValue" : "DataValue"
-    }
-    with open(from_file["filename"], "r") as reader:
-        csv_file = csv.reader(from_file["filename"], delimiter=',')
+    file_name = file_selector.select_files()
+    if(file_name):
+        print("File found")
+    else:
+        print("File cannot be found")
+    
+    with open(file_name, "r") as reader:
+        csv_file = csv.reader(file_name, delimiter=',')
         for row in reader:
-            if from_file["DataValue"] in row:
+            if DATA_VALUE in row:
                 data.append(row)
                 count += 1
-            elif from_file["DataName"] in row:
+            elif DATA_NAME in row:
                 data.append(row)
+                names.append(row)
             else:
                 pass
     reader.close
 
-    rdson.parser(data, from_file, count)
+    which_one = file_selector.which_parser(names, DATA_NAME)
+    
+    if which_one == "RDSon":
+        print("parsing and plotting RDSon")
+        rdson.parser(data, DATA_VALUE, DATA_NAME, count)
+    elif which_one == "IG":
+        print("ig work in progress")
+    elif which_one == "Quadrant":
+        print("3rd quadrant work in progress")
+    else:
+        print("not possible to parse")
+
 
 main()
