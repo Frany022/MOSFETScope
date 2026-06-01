deaf6b2f90c5907940e948eab7fdb4696d53a195
Tibi <bajortibor02@gmail.com>
2026-06-01 10:46:58 +0200

    Update rdson.py

--- rdson.py
+++ rdson.py
@@ -1,33 +1,22 @@
-import csv
 import numpy as np
+import mplcursors
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
 
-def read_csv():
-    data = []
-    count = 0
-    from_file = {
-        "filename": "test3.csv",
-        "DataName" : "DataName",
-        "DataValue" : "DataValue"
-    }
-    with open(from_file["filename"], "r") as reader:
-        csv_file = csv.reader(from_file["filename"], delimiter=',')
-        for row in reader:
-            if from_file["DataValue"] in row:
-                data.append(row)
-                count += 1
-            elif from_file["DataName"] in row:
-                data.append(row)
-            else:
-                pass
-    reader.close
-    type = parser(data, from_file)
-    return data, count, type
 
-def parser(data, from_file):
+def parser(data, from_file, count):
     for i in range(len(from_file["DataName"])):
-        type = data[0].replace(from_file["DataName"] + "," + " ", "")
-        type = type.split(", ")
-    return type
+        types = data[0].replace(from_file["DataName"] + "," + " ", "")
+        types = types.split(", ")
+    nums = [[0 for i in range(len(types))] for j in range(count)]
+    for i in range(count + 1):
+        if i == 0:
+            continue
+        values = data[i].replace(from_file["DataValue"] + "," + " ", "")
+        values = values.split(", ")
+        nums[i - 1] = [float(x) for x in values]
 
-def char():
-    pass
+    char(count, types, nums)
+
+def char(count, types, nums):
+    print(nums)
