2b4753d69104b54e92e3ddba8317a54eef616338
Tibi <bajortibor02@gmail.com>
2026-06-01 10:46:39 +0200

    csv reader in main

--- main.py
+++ main.py
@@ -1,13 +1,29 @@
-import csv
 import rdson
 import igs
 import quadrant
-import numpy as np
-import matplotlib as mpl
+import csv
 
 
 def main():
-    data, count = rdson.read_csv()
-    print(f"{data}")
+    data = []
+    count = 0
+    from_file = {
+        "filename": "test3.csv",
+        "DataName" : "DataName",
+        "DataValue" : "DataValue"
+    }
+    with open(from_file["filename"], "r") as reader:
+        csv_file = csv.reader(from_file["filename"], delimiter=',')
+        for row in reader:
+            if from_file["DataValue"] in row:
+                data.append(row)
+                count += 1
+            elif from_file["DataName"] in row:
+                data.append(row)
+            else:
+                pass
+    reader.close
+
+    rdson.parser(data, from_file, count)
 
 main()
