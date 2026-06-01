56603ea59e08e8c2b697e2ec0a24f0ca7fabcdd8
Tibi <bajortibor02@gmail.com>
2026-06-01 14:41:36 +0200

    Update file_selector.py

--- file_selector.py
+++ file_selector.py
@@ -1,6 +1,15 @@
 import tkinter as tk
 from tkinter import filedialog
 
+required_names = {
+    "RDSon" : {"ids", "vds", "vgs"},
+    "IG": {"vgs"},
+    "Quadrant" : {"ids", "vds"}
+}
+
+rdson_names = {"rdson"}
+ig_names = {"ig", "igabs"}
+
 def select_files():
     root = tk.Tk()
     root.withdraw()
@@ -13,20 +22,23 @@ def select_files():
     return filepath
 
 def which_parser(names, DATA_NAME):
+    names = [n.replace(DATA_NAME + ", ", "").replace("\\n", "").strip().lower() for n in names]
+    names = ", ".join(names).split(", ")
+    names = [n.strip().lower() for n in names]
+    normalized = set(names)
 
-    for i in range(len(names)):
-        names[i] = names[i].replace(DATA_NAME + ", ", "")
-
-    #not the best solution, but works for now 
-    for i in enumerate(names):
-        if i == "Rdson" or "Rdson\n" or "RDSon" or "RDSon\n":
-            return "RDSon"
-        elif i == "IG" or "IGabs":
-            return "IG"
-        else:
-            return "Quadrant"
-
-
-
-
-
+    '''
+    debug
+    print("normalized:", normalized)
+    print("rdson check:", required_names["RDSon"].issubset(normalized), any(n in normalized for n in rdson_names))
+    print("ig check:", required_names["IG"].issubset(normalized), any(n in normalized for n in ig_names))
+    print("quadrant check:", required_names["Quadrant"].issubset(normalized))
+    '''
+    if required_names["RDSon"].issubset(normalized) and any(n in normalized for n in rdson_names):
+        return "RDSon"
+    elif required_names["IG"].issubset(normalized) and any(n in normalized for n in ig_names):
+        return "IG"
+    elif required_names["Quadrant"].issubset(normalized):
+        return "Quadrant"
+    
+    return None
