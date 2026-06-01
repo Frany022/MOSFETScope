a4ca740a716ecb63d43a2d47d3acf82a6a83728b
Tibi <bajortibor02@gmail.com>
2026-06-01 11:18:35 +0200

    refactoring rdson
    it became much faster

--- rdson.py
+++ rdson.py
@@ -3,6 +3,12 @@ import mplcursors
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 
+for_rdson = {
+    "IDS" : "IDS",
+    "VDS" : "VDS",
+    "RDSon" : "rdson",
+    "VGS" : "VGS"
+}
 
 def parser(data, from_file, count):
     for i in range(len(from_file["DataName"])):
@@ -16,7 +22,32 @@ def parser(data, from_file, count):
         values = values.split(", ")
         nums[i - 1] = [float(x) for x in values]
 
+        max_len = max(len(row) for row in nums)
+        for row in nums:
+            while len(row) < max_len:
+                row.append(0)
+
     char(count, types, nums)
 
 def char(count, types, nums):
-    print(nums)
+
+    IDS_index = [i for i, x in enumerate(types) if x == for_rdson["IDS"]]
+    VDS_index = [i for i, x in enumerate(types) if x == for_rdson["VDS"]]
+    RDSon_index = [i for i, x in enumerate(types) if x.lower() == for_rdson["RDSon"] or "rdson\n"]
+    VGS_index = [i for i, x in enumerate(types) if x == for_rdson["VGS"]]
+
+    if not IDS_index:   print("No IDS for char");   return
+    if not VDS_index:   print("No VDS for char");   return
+    if not RDSon_index: print("No rdson for char"); return
+    if not VGS_index:   print("No vgs for char");   return
+
+    nums = np.array(nums)
+
+    IDS = nums[:count, IDS_index].flatten()
+    VDS = nums[:count, VDS_index].flatten()
+    RDSon = nums[:count, RDSon_index].flatten()
+    VGS = nums[:count, VGS_index].flatten()
+
+    print(f"IDS: {IDS} \n VDS: {VDS} \n RDSon: {RDSon}")
+    
+
