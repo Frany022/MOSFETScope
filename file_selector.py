6d7dec946979dbe6f0a50bacdc8fe2d7b7f032d3
Tibi <bajortibor02@gmail.com>
2026-06-04 09:38:55 +0200

    Update file_selector.py

--- file_selector.py
+++ file_selector.py
@@ -21,12 +21,32 @@ def select_files():
 
     return filepath
 
-def which_parser(names, DATA_NAME):
+def parser(data, DATA_VALUE: str, DATA_NAME: str, count: int):
+    for i in range(len(DATA_NAME)):
+        types = data[0].replace(DATA_NAME + "," + " ", "")
+        types = types.split(", ")
+    nums = [[0 for i in range(len(types))] for j in range(count)]
+
+    for i in range(count + 1):
+        if i == 0:
+            continue
+
+        values = data[i].replace(DATA_VALUE + "," + " ", "")
+        values = values.split(", ")
+        nums[i - 1] = [float(x) for x in values]
+
+        max_len = max(len(row) for row in nums)
+        for row in nums:
+            while len(row) < max_len:
+                row.append(0)
+
+    return types, nums
+
+def which_char(names, DATA_NAME):
     names = [n.replace(DATA_NAME + ", ", "").replace("\\n", "").strip().lower() for n in names]
     names = ", ".join(names).split(", ")
     names = [n.strip().lower() for n in names]
     normalized = set(names)
-
     '''
     debug
     print("normalized:", normalized)
